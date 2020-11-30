02ddfec4c1d58864a9b267269b91b0e4f5bac2fe
Alex Báez <baezdalex@gmail.com>
2020-11-30 16:47:46 -0600

    ✨ Components (Layout, buttons) and Custom Hook

---
+++ src/components/design/buttons.js
@@ -0,0 +1,70 @@
+import styled from "styled-components"
+
+export const ButtonOutline = styled.button`
+  border-radius: 25px;
+  border: 1px solid hsl(322, 100%, 66%);
+  background: transparent;
+  color: hsl(322, 100%, 66%);
+  padding: 0.3rem 1rem;
+  cursor: pointer;
+  transition: 0.5s ease;
+  font-size: 0.8rem;
+  &:hover {
+    background: hsl(322, 100%, 66%);
+    color: white;
+  }
+  &:focus {
+    outline: 0;
+  }
+  
+  @media (min-width: 768px) {
+    padding: 0.3rem 1rem;
+    font-size: 1rem;
+  }
+`
+
+export const ButtonPrimary = styled.button`
+  border-radius: 25px;
+  background: hsl(322, 100%, 66%);
+  border:none;
+  color: white;
+  padding: 0.8rem 2.5rem;
+  font-size: 0.9rem;
+  font-weight: 600;
+  cursor: pointer;
+  transition: 0.5s ease;
+  &:hover {
+    transform: translateY(-10px);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
+  }
+
+  &:focus{
+    outline:0;
+  }
+
+  @media (min-width: 768px) {
+    padding: 1rem 4rem;
+    font-size: 1rem;
+  }
+`
+
+export const ButtonSubmit = styled.button.attrs({ type: "submit" })`
+  border-radius: 5px;
+  border: none;
+  background: hsl(322, 100%, 66%);
+  color: white;
+  padding: 0.6rem 2.1rem;
+  font-size: 1rem;
+  font-family: "Open Sans", sans-serif;
+  transition: 0.5s ease;
+  cursor:pointer;
+  &:hover {
+    background: green;
+    color: white;
+  }
+
+  @media (min-width: 768px) {
+    padding: 0.6rem 1.5rem;
+    font-size: 0.8rem;
+  }
+`
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,11 +8,35 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled ,{ createGlobalStyle } from "styled-components"
 
 import Header from "./header"
-import "./layout.css"
+import Footer from "./footer"
+
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding:0;
+    margin:0;
+    box-sizing: border-box;
+    font-family: 'Open Sans', sans-serif;
+    font-size:1rem;
+  }
+  h1,h2,h3,h4{
+    font-family: 'Poppins', sans-serif;
+    margin:0;
+  }
+  p {
+  margin:0;
+  }
+`
+const Main = styled.main`
+  margin:0 auto;
+`
+
 
 const Layout = ({ children }) => {
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,23 +49,10 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <GlobalStyle />
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <Main>{children}</Main>
+      <Footer />
     </>
   )
 }
