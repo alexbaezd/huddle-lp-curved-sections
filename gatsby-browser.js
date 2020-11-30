103f05effe232e9d92454bc217a39298c4124775
Alex Báez <baezdalex@gmail.com>
2020-11-30 16:46:01 -0600

    🎉 Begin a Project: (Install  dependencies, add assets)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,5 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onClientEntry = () => {
+  console.log("Hello World")
+  console.log("Hey Dev")
+  console.log("Repo => ");
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Huddle landing page with curved sections`,
+    description: `Frontend Mentor challenges allow you to improve your skills in a real-life workflow.`,
+    author: `@alexbaezd`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,22 +13,19 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Huddle Landing Page With Curved Sections`,
+        short_name: `Huddle`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ff52bf`,
+        theme_color: `#ff52bf`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-components`,
   ],
 }
