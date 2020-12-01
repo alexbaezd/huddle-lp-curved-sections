c0464a0d66c335df3da1d9858b242b1da93c7af0
Alejandro Báez <baezdalex@gmail.com>
2020-12-01 12:53:25 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         background_color: `#ff52bf`,
         theme_color: `#ff52bf`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png `, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
